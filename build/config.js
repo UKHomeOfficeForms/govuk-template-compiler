c31a431d78eedd5f47f8fe882c0bfb42f72f8ff5
Leonard Martin <leonard.martin@digital.cabinet-office.gov.uk>
2015-02-06 10:41:50 +0000

    Add template compile script
    npm postinstall script to pull govuk template from module and compile into a more usable form which allows for passing template variables up to parent templates

---
+++ build/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  assetPath: '{{assetPath}}',
+  afterHeader: '{{$afterHeader}}{{/afterHeader}}',
+  bodyClasses: '{{$bodyClasses}}{{/bodyClasses}}',
+  bodyEnd: '{{$bodyEnd}}{{/bodyEnd}}',
+  content: '{{$main}}{{/main}}',
+  cookieMessage: '{{$cookieMessage}}{{/cookieMessage}}',
+  footerSupportLinks: '{{$footerSupportLinks}}{{/footerSupportLinks}}',
+  footerTop: '{{$footerTop}}{{/footerTop}}',
+  head: '{{$head}}{{/head}}',
+  headerClass: '{{$headerClass}}{{/headerClass}}',
+  insideHeader: '{{$insideHeader}}{{/insideHeader}}',
+  pageTitle: '{{$pageTitle}}{{/pageTitle}}',
+  propositionHeader: '{{$propositionHeader}}{{/propositionHeader}}'
+};
