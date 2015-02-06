e45de6f6819060c668796f6086bb77c99f1fbd9e
Leonard Martin <leonard.martin@digital.cabinet-office.gov.uk>
2015-02-06 12:05:38 +0000

    Bring code implementation in line with documentation
    Remove option for passing assetpath as an explicit parameter, just load oit from options instead

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
 var path = require('path'),
-    static = require('serve-static');
+    servestatic = require('serve-static');
 
 module.exports = {
-    setup: function (app, assetpath, options) {
-
-        if (arguments.length === 2 && typeof assetpath === 'object') {
-            options = assetpath;
-            assetpath = '';
-        }
+    setup: function (app, options) {
 
         options = options || {};
-        assetpath = assetpath || options.path || '/govuk-assets';
+        options.path = options.path || '/govuk-assets';
 
-        app.use(assetpath, static(path.join(__dirname, './node_modules/govuk_template_mustache/assets'), options));
+        app.use(options.path, servestatic(path.join(__dirname, './node_modules/govuk_template_mustache/assets'), options));
         app.use(function (req, res, next) {
-            res.locals.govukAssetPath = assetpath + '/';
+            res.locals.govukAssetPath = options.path + '/';
             res.locals.partials = res.locals.partials || {};
             res.locals.partials['govuk-template'] = path.resolve(__dirname, './govuk_template');
             next();
