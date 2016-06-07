c5934dac0dd6c43a6787a73cd77c7db2d4df4b37
Paul Winkler <paul.winkler@homeoffice.gov.uk>
2016-06-07 17:28:21 +0100

    Add default text and override tags to template

--- build/config.js
+++ build/config.js
@@ -11,5 +11,9 @@ module.exports = {
   headerClass: '{{$headerClass}}{{/headerClass}}',
   insideHeader: '{{$insideHeader}}{{/insideHeader}}',
   pageTitle: '{{$pageTitle}}{{/pageTitle}}',
-  propositionHeader: '{{$propositionHeader}}{{/propositionHeader}}'
+  propositionHeader: '{{$propositionHeader}}{{/propositionHeader}}',
+  skipLinkMessage: '{{$skipLinkMessage}}Skip to main content{{/skipLinkMessage}}',
+  globalHeaderText: '{{$globalHeaderText}}GOV.UK{{/globalHeaderText}}',
+  licenceMessage: '{{$licenceMessage}}<p>All content is available under the <a href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/" rel="license">Open Government Licence v3.0</a>, except where otherwise stated</p>{{/licenceMessage}}',
+  crownCopyrightMessage: '{{$crownCopyrightMessage}}© Crown copyright{{/crownCopyrightMessage}}'
 };
